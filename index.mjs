3f6c6fa19b6af8473bd2954e11e13c6d42a1d24e
James <knotsite@gmail.com>
2022-12-24 13:20:02 +0700

    starter code

---
+++ index.mjs
@@ -0,0 +1,46 @@
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({
+  auth: "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",
+});
+
+// Goal: to delete repo created by cohort 2022.1 and 2022.2
+// Steps
+// 1) list all the repos created by cohort 2022.1 and 2022.2
+// 2) delete each repo by repo name
+
+// List all repos
+
+// const results = await octokit.request(`GET /orgs/{org}/repos`, {
+//   org: "techupth",
+// });
+
+// console.log(
+//   results.data.map((repo) => {
+//     return {
+//       id: repo.id,
+//       node_id: repo.node_id,
+//       full_name: repo.full_name,
+//     };
+//   })
+// );
+
+// Delete
+
+// await octokit.request("DELETE /repos/{owner}/{repo}", {
+//   owner: "techupth",
+//   repo: "css-hh-pseudo-class-and-element-kant-nat",
+// });
+
+// Get a Repo
+
+// const result = await octokit.request("GET /repos/{owner}/{repo}", {
+//   owner: "techupth",
+//   // repo: "merry-match",
+//   repo: "react-hh-data-fetching-products-naisu-touch",
+// });
+
+console.log(result);
