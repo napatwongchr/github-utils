f646a69a5eb1edb3efb2efe652955318f1133016
James <knotsite@gmail.com>
2023-02-03 14:10:34 +0700

    add delete code

--- index.mjs
+++ index.mjs
@@ -20,44 +20,59 @@ import {
 
 const org = "techupth";
 
-const teamList = await getTeams({
+let teamList = await getTeams({
   org,
-  per_page: 10,
+  per_page: 1,
 });
 
-teamList.data
-  // .filter((team) => team.slug !== "Aun-Pok")
-  .forEach(async (team) => {
-    const teamRepos = await getTeamRepos({
-      org,
-      team_slug: team.slug,
+let hasTeam = !teamList.length;
+
+while (hasTeam) {
+  // 1) Get team repo
+  teamList.data
+    // .filter((team) => team.slug !== "Aun-Pok")
+    .forEach(async (team) => {
+      const teamRepos = await getTeamRepos({
+        org,
+        team_slug: team.slug,
+      });
+
+      const regex = /GitHub Classroom/i;
+
+      // 2) Delete team repo
+      teamRepos.data
+        .filter((repo) => {
+          return regex.test(repo.description);
+        })
+        .map((repo) => {
+          return {
+            name: repo.name,
+            description: repo.description,
+          };
+        })
+        .forEach(async (repo) => {
+          // await deleteTeamRepo( {
+          //   org: org,
+          //   team_slug: team.slug,
+          //   owner: org,
+          //   repo: "REPO",
+          // })
+          console.log(`removed repo: ${repo.name} from team: ${team.name}`);
+
+          // 3) Delete team
+          // await deleteTeam({
+          //   org: org,
+          //   team_slug: team.slug,
+          // })
+          console.log(`removed team: ${team.name} \n`);
+        });
     });
 
-    const regex = /GitHub Classroom/i;
-
-    teamRepos.data
-      .filter((repo) => {
-        return regex.test(repo.description);
-      })
-      .map((repo) => {
-        return {
-          name: repo.name,
-          description: repo.description,
-        };
-      })
-      .forEach(async (repo) => {
-        // await deleteTeamRepo( {
-        //   org: org,
-        //   team_slug: team.slug,
-        //   owner: org,
-        //   repo: "REPO",
-        // })
-        console.log(`removed repo: ${repo.name} from team: ${team.name}`);
-
-        // await deleteTeam({
-        //   org: org,
-        //   team_slug: team.slug,
-        // })
-        console.log(`removed team: ${team.name} \n`);
-      });
+  // 4) Get a new team list
+  teamList = await getTeams({
+    org,
+    per_page: 1,
   });
+
+  hasTeam = !teamList.length;
+}
